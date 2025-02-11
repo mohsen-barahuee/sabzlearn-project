00fbc3373ecf182698ac17aa96420fbd1661066c
Mohsen barahuee <nakel3920@gmail.com>
2025-02-11 13:58:12 +0330

    create UserModel

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const shcema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["ADMIN", "USER"],
+        default: "USER"
+    }
+}, { timestamps: true })
+
+const model = mongoose.model("User",shcema)
+
+module.exports = model
