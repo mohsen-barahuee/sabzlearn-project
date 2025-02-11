f0f489b1aa763ac373f056d0e392a1eb30ffdaf0
Mohsen barahuee <nakel3920@gmail.com>
2025-02-11 14:17:16 +0330

    make users model and validators

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require("express")
-
+const authRouter = require("./routes/auth")
+const cors = require('cors')
+const path = require('path')
+const bodyParses = require("body-parser")
 const app = express()
+
+app.use(
+    '/courses/covers',
+    express.static(path.join(__dirname, "public", 'courses', 'covers')))
+
+
+app.use(cors())
+app.use(bodyParses.json())
+app.use(bodyParses.urlencoded({extended:false}))
+
+app.use('/v1/auth', authRouter)
 
 module.exports = app
---
+++ controllers/v1/auth.js
@@ -0,0 +1,22 @@
+const userModel = require('../../models/user')
+
+
+exports.register = async (req, res) => {
+
+}
+exports.login = async (req, res) => {
+
+}
+exports.getME = async (req, res) => {
+
+}
+
+
+
+
+
+
+
+
+
+
