13679f6f21ee59a63c6b11dafc782293fd190e09
Mohsen barahuee <nakel3920@gmail.com>
2025-02-14 01:20:51 +0330

    fix some issuce

--- controllers/v1/user.js
+++ controllers/v1/user.js
@@ -8,7 +8,7 @@ exports.banUser = async (req, res) => {
     // FIND THE USER AND RETURN THE COLLECTION IN OBJECT
     const mainUser = await userModel.findOne({ _id: req.params.id }).lean()
     const banUserResult = await banUserModel.create({ phone: mainUser.phone })
-    if (banUserModel) {
+    if (banUserResult) {
         return res.status(200).json({ message: "User ban successfull" })
     }
     return res.status(500).json({ message: "Server Error !!" })
@@ -57,10 +57,7 @@ exports.changeRole = async (req, res) => {
 
     return res.json({ message: "User role has been change" })
 
-
-
 }
-
 
 exports.updateUser = async (req,res) => {
 
